1c33e3f8aad5db3993555db269e0ad80637342ca
Timofey <ktkozlov@yandex.ru>
2019-03-26 10:36:46 +0500

    Change a statistic style to material

--- script2.js
+++ script2.js
@@ -1,18 +1,39 @@
-google.load("visualization", "1", {packages: ["corechart"]});
-google.setOnLoadCallback(drawChart);
+google.charts.load('current', {'packages': ['corechart', 'bar']});
+google.charts.setOnLoadCallback(drawStuff);
 
-function drawChart() {
+function drawStuff() {
+
+    var button = document.getElementById('change-chart');
+    var chartDiv = document.getElementById('chart_div');
+
     var data = google.visualization.arrayToDataTable([
         ['День', 'Настроение'],
         [24, -0.5],
         [25, 0.1],
         [26, 0.7]
     ]);
-    var options = {
-        title: 'Настроение в Екатеринбурге',
-        hAxis: {title: 'День'},
-        vAxis: {title: 'Уровень настроения'}
+
+    var materialOptions = {
+        width: 900,
+        chart: {
+            title: 'Уровень настроения в Екатеринбурге',
+            subtitle: ''
+        },
+        series: {
+            0: {axis: 'Настроение'}, // Bind series 0 to an axis named 'distance'.
+        },
+        axes: {
+            y: {
+                distance: {label: 'Уровень настроения'}, // Left y-axis.
+                // brightness: {side: 'right', label: 'apparent magnitude'} // Right y-axis.
+            }
+        }
     };
-    var chart = new google.visualization.ColumnChart(document.getElementById('level_mood'));
-    chart.draw(data, options);
-}
+
+    function drawMaterialChart() {
+        var materialChart = new google.charts.Bar(chartDiv);
+        materialChart.draw(data, google.charts.Bar.convertOptions(materialOptions));
+    }
+
+    drawMaterialChart();
+};
